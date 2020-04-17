aefb2b99a131a81152ce6a0b07b45871c740b9bf
matthew-heideman <heidemanmr@yahoo.com>
2020-04-17 12:59:41 -0500

    wip

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,17 +1,58 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import {NavItem, Nav, NavLink, TabContent, TabPane} from 'reactstrap';
 import './App.css';
-
+import classnames from 'classnames';
+import Character from './components/Character';
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
+  const [data, setData] = useState([]);
+  const [activeTab, setActiveTab] = useState('1');
+  const toggle = tab => {
+    if(activeTab !== tab) setActiveTab(tab);
+  }
+  const url = 'https://rickandmortyapi.com/api/'
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-
+  useEffect(() => {
+    axios
+      .get(`${url}character`)
+      .then(response => {
+        console.log(response);
+        setData(response.data.results);
+      })
+      .catch(error => {
+        console.log('The data was not retrieved', error);
+      })
+  }, [])
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <Nav tabs>
+        <NavItem>
+          <NavLink
+            className={classnames({ active: activeTab === '1' })}
+            onClick={() => { toggle('1'); }}>
+            Characters
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink
+            className={classnames({ active: activeTab === '2' })}
+            onClick={() => { toggle('2'); }}>
+              More Stuf
+          </NavLink>
+        </NavItem>
+      </Nav>
+      <TabContent activeTab={activeTab} >
+        <TabPane tabId="1">
+          <Character data = {data} />
+        </TabPane>
+        <TabPane tabId='2'>
+          
+        </TabPane>
+      </TabContent>
     </div>
   );
 }
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,36 @@
 // Write your Character component here
+import React from 'react';
+import {Card, CardHeader, CardBody, CardTitle, CardSubtitle, CardText} from 'reactstrap';
+
+function Character(props){
+    console.log(props.data)
+ return(
+    <div>
+        {props.data.forEach(item => {
+        return  <Card body inverse style={{ backgroundColor: '#333', borderColor: '#333' }} key = {item.id} >
+                    <CardHeader>
+                        <CardTitle>
+                            Name: {item.name}
+                        </CardTitle>
+                    </CardHeader>
+                    <CardBody>
+                        <CardSubtitle>
+                            Staus: {item.status}
+                        </CardSubtitle>
+                        <CardText>
+                            Species: {item.species}
+                        </CardText> 
+                        <CardText>
+                            Homeworld and Universe (if applicable): {item.orign.name}
+                        </CardText>
+                        <CardText>
+                            Episode Count: {item.episodes.length}
+                        </CardText>
+                    </CardBody>
+                </Card>
+        })
+        }
+    </div>
+ )
+}
+export default Character;
