11ac9c1c3674d4bd86730474b5d0ffbe571b35f5
matthew-heideman <heidemanmr@yahoo.com>
2020-04-17 15:23:12 -0500

    wip

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,36 +1,37 @@
 // Write your Character component here
 import React from 'react';
-import {Card, CardHeader, CardBody, CardTitle, CardSubtitle, CardText} from 'reactstrap';
+import { Col, Card, CardHeader, CardBody, CardTitle, CardSubtitle, CardText, CardImg} from 'reactstrap';
 
 function Character(props){
     console.log(props.data)
  return(
-    <div>
-        {props.data.forEach(item => {
-        return  <Card body inverse style={{ backgroundColor: '#333', borderColor: '#333' }} key = {item.id} >
-                    <CardHeader>
-                        <CardTitle>
-                            Name: {item.name}
-                        </CardTitle>
-                    </CardHeader>
-                    <CardBody>
-                        <CardSubtitle>
-                            Staus: {item.status}
-                        </CardSubtitle>
-                        <CardText>
-                            Species: {item.species}
-                        </CardText> 
-                        <CardText>
-                            Homeworld and Universe (if applicable): {item.orign.name}
-                        </CardText>
-                        <CardText>
-                            Episode Count: {item.episodes.length}
-                        </CardText>
-                    </CardBody>
-                </Card>
-        })
-        }
-    </div>
- )
+    <Col sm = '6'>
+    {props.data.map(item => {
+    return  <Card  body inverse style={{ backgroundColor: '#333', borderColor: '#333' }} key = {item.id} >
+                <CardImg src={item.image} alt = {item.name} />
+                <CardHeader>
+                    <CardTitle>
+                        Name: {item.name}
+                    </CardTitle>
+                </CardHeader>
+                <CardBody>
+                    <CardSubtitle>
+                        Staus: {item.status}
+                    </CardSubtitle>
+                    <CardText>
+                        Species: {item.species}
+                    </CardText> 
+                    <CardText>
+                        Homeworld and Universe (if applicable): {item.origin.name}
+                    </CardText>
+                    <CardText>
+                        Episode Count: {item.episode.length}
+                    </CardText>
+                </CardBody>
+            </Card>
+    })
+    }
+    </Col>)
+ 
 }
 export default Character;
